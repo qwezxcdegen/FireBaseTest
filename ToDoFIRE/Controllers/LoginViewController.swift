2d462b8fba04ffaea0e80238f2ec7c1b67a4183f
qwezxcdegen <ifomstep@icloud.com>
2023-04-02 21:19:37 +0500

    Fix

--- ToDoFIRE/Controllers/LoginViewController.swift
+++ ToDoFIRE/Controllers/LoginViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 final class LoginViewController: UIViewController {
 
@@ -16,12 +17,34 @@ final class LoginViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        warningLabel.alpha = 0
+        FirebaseApp.configure()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true)
     }
 
     @IBAction func loginPressed(_ sender: UIButton) {
+        guard let email = emailTF.text, let password = passwordTF.text, email != "", password != "" else {
+            displayWarningLabel(with: "Data is incorrect")
+            return
+        }
+        FirebaseApp.
     }
     
     @IBAction func registerPressed(_ sender: UIButton) {
+        displayWarningLabel(with: "qwe")
+    }
+    
+    private func displayWarningLabel(with text: String) {
+        warningLabel.text = text
+        UIView.animate(withDuration: 3, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseIn, animations: {
+            self.warningLabel.alpha = 1
+        }) { _ in
+            self.warningLabel.alpha = 0
+        }
     }
 }
 
